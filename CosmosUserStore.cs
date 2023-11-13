908c68e8e689df3f82a4ca61c9396b2441331cf1
Jean-Michel Nadeau <jeanmichel.nadeau@gmail.com>
2023-11-13 13:30:47 -0500

    Create CosmosUserStore.cs

---
+++ CosmosUserStore.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Super_Cartes_Infinies.Data
+{
+    public class CosmosUserStore : UserStore<IdentityUser, IdentityRole, ApplicationDbContext>
+    {
+        public CosmosUserStore(ApplicationDbContext context, IdentityErrorDescriber describer = null) : base(context, describer)
+        {
+        }
+
+        public override async Task<IList<string>> GetRolesAsync(IdentityUser user,
+                                                                CancellationToken cancellationToken =
+                                                                    new CancellationToken())
+        {
+            // force the enumerable to execute rather than joining
+            var roles = await Context.Set<IdentityRole>().ToDictionaryAsync(r => r.Id, cancellationToken);
+
+            var userRolesIds = await Context.Set<IdentityUserRole<string>>()
+                                         .Where(ur => ur.UserId == user.Id)
+                                         .Select(ur => ur.RoleId)
+                                         .ToListAsync(cancellationToken);
+
+            return userRolesIds.Select(ur => roles[ur].Name).ToList();
+        }
+    }
+}
